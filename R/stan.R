08933d93814353127428040d74bcdc59163b4803
Masatoshi Katabuchi <mattocci27@gmail.com>
2022-04-11 00:59:45 +0900

    added heteroskedasticity

--- R/stan.R
+++ R/stan.R
@@ -2,20 +2,24 @@ clean_stan_data <- function(sp_mean, interaction = TRUE) {
   x <- cbind(
     intercept = rep(1, nrow(sp_mean)),
     #lma_disc = sp_mean$lma_disc |> log10() |> scale(),
-    la = sp_mean$la |> log() |> scale(),
-    lt = sp_mean$lt |> log() |> scale()
+    #la = sp_mean$la |> log() |> scale() |> as.numeric(),
+    #lt = sp_mean$lt |> log() |> scale() |> as.numeric()
+    la = sp_mean$la |> log() |> as.numeric(),
+    lt = sp_mean$lt |> log() |> as.numeric()
   )
-  if (interaction) {
+  if (interaction){
     x <- cbind(x, x[, 2] * x[, 3])
     colnames(x) <- c("intercept", "la", "lt", "lalt")
-  } else {
-    colnames(x) <- c("intercept", "la", "lt")
   }
+  #log_lma_disc <- sp_mean$lma_disc |> log() |> scale() |> as.numeric()
+  # log_lma_leaf <- sp_mean$lma_leaf |> log() |> scale() |> as.numeric()
+  log_lma_disc <- sp_mean$lma_disc |> log() |> as.numeric()
+  log_lma_leaf <- sp_mean$lma_leaf |> log() |> as.numeric()
   list(
     N = nrow(sp_mean),
     K = ncol(x),
-    log_y = log(sp_mean$lma_leaf),
-    log_lma_disc = sp_mean$lma_disc |> log() |> scale() |> as.numeric(),
+    log_y = log_lma_leaf,
+    log_lma_disc = log_lma_disc,
     x = x)
 }
 
@@ -23,11 +27,13 @@ create_dummy_data <- function(n) {
   xx1 <- rnorm(n)
   xx2 <- rnorm(n)
   xx3 <- rnorm(n)
+  gamma <- c(-1.4, -0.5, 0.5)
+  beta <- c(0, 0.5, 0.5)
   x <- cbind(rep(1, n), xx2, xx3)
-  sig <- 1.41 + 1 * xx2 - 1 * xx3
-  sig <- rnorm(n, sig, 0.2)
-  yy <- rnorm(n, 0.3 + 0.9 * xx1, exp(sig))
-  #lm(yy ~ xx1) |> summary()
+  log_mu <- as.numeric(x %*% beta) + xx1
+  log_sig_hat <- as.numeric(x %*% gamma)
+  log_sig <- rnorm(n, log_sig_hat, 0.2)
+  yy <- rnorm(n, xx1, exp(log_sig))
   list(
     N = n,
     K = ncol(x),
--- stan/model.stan
+++ stan/model.stan
@@ -1,40 +1,38 @@
 data{
   int<lower=0> N;
   int<lower=0> K;
-  vector[N] log_y; // whole-leaf lma
-  vector[N] log_lma_disc; //
+  vector[N] log_y; // whole-leaf LMA / disc LMA
+  vector[N] log_lma_disc; // whole-leaf LMA / disc LMA
   matrix[N, K] x; // predictors
 }
 
 parameters{
-  vector[2] beta;
+  vector[K] beta;
   vector[K] gamma;
-  //vector[N] log_sigma;
-  real<lower=0> tau;
+  real<lower=0> omega;
   vector[N] log_sigma;
 }
 
 model{
-  // vector[N] log_sigma;
-  // log_sigma = x * gamma;
-  vector[N] log_sigma_hat;
-  log_sigma_hat = x * gamma;
+  vector[N] log_mu;
+  vector[N] sigma;
+  sigma = exp(log_sigma);
+  log_mu = x * beta + log_lma_disc ;
   beta ~ normal(0, 5);
   gamma ~ normal(0, 5);
-  tau ~ cauchy(0, 5);
-  log_sigma ~ normal(log_sigma_hat, tau);
-  log_y ~ normal(beta[1] + beta[2] * log_lma_disc, exp(log_sigma));
-  //log_y ~ normal(log_lma_disc, exp(log_sigma));
+  log_sigma ~ normal(x * gamma, omega);
+  omega ~ cauchy(0, 5);
+  log_y ~ normal(log_mu, sigma);
 }
 
 // for model selection
 generated quantities {
   vector[N] log_lik;
-  // vector[N] log_sigma;
-  // log_sigma = x * gamma;
+  vector[N] log_mu;
+  vector[N] sigma;
+  sigma = exp(log_sigma);
+  log_mu = x * beta + log_lma_disc ;
   for (n in 1:N) {
-    //log_lik[n] = normal_lpdf(log_y[n] | log_lma_disc[n],
-    log_lik[n] = normal_lpdf(log_y[n] | beta[1] + beta[2] * log_lma_disc[n],
-                                        exp(log_sigma[n]));
+    log_lik[n] = normal_lpdf(log_y[n] | log_mu[n], sigma[n]);
   }
 }
